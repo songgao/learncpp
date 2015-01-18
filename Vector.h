bc8db02996ad81d29a111cf21136ee433efbb2a2
Xinyu Zhao <xinyu.zhao@gao.io>
2015-01-18 15:42:05 -0600

    initial project version

---
+++ Vector.h
@@ -0,0 +1,15 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+class Vector{
+    public:
+        Vector(int s);
+        double & operator[](int i);
+        int size();
+    private:
+        double* elem;
+        int sz;
+};
+
+#endif
+
