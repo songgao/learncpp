8e2da3b1d9daa99d5ec17b0cb4b438bf2564962e
Xinyu Zhao <xinyu.zhao@gao.io>
2015-01-18 16:22:31 -0600

    Fix Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -2,16 +2,10 @@
 #include <stdexcept>
 
 Vector::Vector(int s) {
-    if (s < 0) {
-        throw std::length_error{};
-    }
     elem = new double[s];
     sz  = s;
 }
 double & Vector::operator[](int i) {
-    if (i < 0 || i >= sz) {
-        throw std::out_of_range{"Vector::operator[]"};
-    }
     return elem[i];
 }
 int Vector::size() {return sz;}
